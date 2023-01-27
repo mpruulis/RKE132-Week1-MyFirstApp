b8702aa24f16fe657903f2d20af892c0e6b29124
Martin Pruuliste <mpruulis@tktk.ee>
2023-01-27 17:46:45 +0200

    create new app

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See on kommentaar minu koodis
+
+// rakendus küsib kasutjalt tema nime
+Console.WriteLine("Sisesta oma nimi: "); //Output
+
+// string = sõne
+string userName = Console.ReadLine(); //Input
+
+// rakendus tervitab kasutajat nime pidi
+Console.WriteLine("Hello, " + userName + "!"); //Output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!"); //Output
